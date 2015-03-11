ef324888d2ba89d80233b0e9cb0c615e820a7849
Gilles Khouzam <gillesk@microsoft.com>
2015-03-11 16:05:24 -0700

    Adding proper copyright headers and licensing under Apache 2.0

--- FFmpegSample/MediaPlayerCS/MediaPlayerCS.WindowsPhone/MainPage.xaml.cs
+++ FFmpegSample/MediaPlayerCS/MediaPlayerCS.WindowsPhone/MainPage.xaml.cs
@@ -18,12 +18,7 @@
 using FFmpeg;
 
 using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.Media.Core;
 using Windows.Storage;
 using Windows.Storage.Pickers;
@@ -31,10 +26,6 @@ using Windows.Storage.Streams;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 namespace MediaPlayerCS

4bef9760a8a6a3063932190c5a354c6e5d01c762
Gilles Khouzam <gillesk@microsoft.com>
2015-05-06 13:01:07 -0700

    Update FFmpegInterop for Win10 DP7 Remove AppInsights dependencies

--- FFmpegSample/SamplesWin10/MediaPlayerCS/App.xaml.cs
+++ FFmpegSample/SamplesWin10/MediaPlayerCS/App.xaml.cs
@@ -32,7 +32,6 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using Microsoft.ApplicationInsights;
 
 namespace MediaPlayerCS
 {
@@ -41,11 +40,6 @@ namespace MediaPlayerCS
     /// </summary>
     sealed partial class App : Application
     {
-        /// <summary>
-        /// Allows tracking page views, exceptions and other telemetry through the Microsoft Application Insights service.
-        /// </summary>
-        public TelemetryClient TelemetryClient = new TelemetryClient();
-
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().

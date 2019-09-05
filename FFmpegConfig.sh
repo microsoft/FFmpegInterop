1a0a2c8d840082c1cac1b2c4765f3d751f5a4cf5
Brian Beecher <brbeec@microsoft.com>
2019-09-05 13:42:02 -0700

    Whitespace changes

--- FFmpegConfig.sh
+++ FFmpegConfig.sh
@@ -10,7 +10,7 @@ common_settings=" \
     --disable-dxva2 \
     --enable-shared \
     --enable-cross-compile \
-	--extra-cflags=\"-MD -DWINAPI_FAMILY=WINAPI_FAMILY_APP -D_WIN32_WINNT=0x0A00\" \
+    --extra-cflags=\"-MD -DWINAPI_FAMILY=WINAPI_FAMILY_APP -D_WIN32_WINNT=0x0A00\" \
     --extra-ldflags=\"-APPCONTAINER WindowsApp.lib\" \
     "
 
@@ -18,36 +18,36 @@ common_settings=" \
 arch="${1,,}"
 
 if [ -z $arch ]; then
-	echo "ERROR: No architecture set!" 1>&2
+    echo "ERROR: No architecture set!" 1>&2
     exit 1
 elif [ $arch == "x86" ]; then
-	architecture_settings="
-		--arch=x86 \
+    architecture_settings="
+        --arch=x86 \
         --prefix=../../Build/$arch \
-	"
+        "
 elif [ $arch == "x64" ]; then
-	architecture_settings="
-		--arch=x86_64 \
+    architecture_settings="
+        --arch=x86_64 \
         --prefix=../../Build/$arch \
-	"
+        "
 elif [ $arch == "arm" ]; then
     architecture_settings="
-		--arch=arm \
+        --arch=arm \
         --as=armasm \
         --cpu=armv7 \
         --enable-thumb \
         --extra-cflags=\"-D__ARM_PCS_VFP\" \
         --prefix=../../Build/$arch \
-	"
+        "
 elif [ $arch == "arm64" ]; then
     architecture_settings="
-		--arch=arm64 \
+        --arch=arm64 \
         --as=armasm \
         --cpu=armv7 \
         --enable-thumb \
         --extra-cflags=\"-D__ARM_PCS_VFP\" \
         --prefix=../../Build/$arch \
-	"
+        "
 else
     echo "ERROR: $arch is not a valid architecture!" 1>&2
     exit 1

005e0c7a0d188e15f3467d7620c434be60f83140
Brian Beecher <brbeec@microsoft.com>
2025-09-30 13:02:41 -0700

    Kill MSYS2 processes before updating packages

--- SetUpFFmpegBuildEnvironment.ps1
+++ SetUpFFmpegBuildEnvironment.ps1
@@ -72,15 +72,15 @@ $msys2_shell = "$msys2_root\msys2_shell.cmd"
 Write-Host "Modifying $msys2_shell..."
 (Get-Content $msys2_shell).replace('rem set MSYS2_PATH_TYPE=inherit', 'set MSYS2_PATH_TYPE=inherit') | Set-Content $msys2_shell
 
-# Update core packages
+# Update packages - the first pass updates core packages and the second pass updates the remaining non-core packages
 Write-Host 'Updating packages...'
-Start-Process -Wait $msys2_shell -ArgumentList '-c "pacman -Syuu --noconfirm"'
+for ($i = 0; $i -lt 2; $i++)
+{
+    # Close all MSYS2 processes
+    & taskkill /FI 'MODULES eq msys-2.0.dll' /F
 
-# Close all MSYS2 processes
-& taskkill /FI 'MODULES eq msys-2.0.dll' /F
-
-# Update the remaining non-core packages
-Start-Process -Wait $msys2_shell -ArgumentList '-c "pacman -Syuu --noconfirm"'
+    Start-Process -Wait $msys2_shell -ArgumentList '-c "pacman -Syuu --noconfirm"'
+}
 
 # Install additional packages 
 $packages = @('make', 'gcc', 'diffutils', 'nasm')

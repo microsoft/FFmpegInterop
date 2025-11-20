525e36942ed03323e6041ccb8f25decadca6e01d
Brian Beecher <brbeec@microsoft.com>
2025-11-20 04:41:26 -0800

    Try --ignore pacman. Enable verbose and debug logging.

--- SetUpFFmpegBuildEnvironment.ps1
+++ SetUpFFmpegBuildEnvironment.ps1
@@ -79,7 +79,7 @@ for ($i = 0; $i -lt 2; $i++)
     # Close all MSYS2 processes
     & taskkill /FI 'MODULES eq msys-2.0.dll' /F
 
-    Start-Process -Wait $msys2_shell -ArgumentList '-c "pacman -Syu --noconfirm"'
+    Start-Process -Wait $msys2_shell -ArgumentList '-c "pacman -Syuu --noconfirm --verbose --debug --ignore pacman"'
 }
 
 # Install additional packages 
